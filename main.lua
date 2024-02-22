e4bd38ca32ae10aefaf443b27ef2851f11a020c8
Raphael Laurentino da Silva <77465404+RaphaelTW@users.noreply.github.com>
2024-02-22 13:51:53 -0300

    tratando a colisão

--- main.lua
+++ main.lua
@@ -1,15 +1,29 @@
 LARGURA_TELA = 320
 ALTURA_TELA = 480
 MAX_METEORO = 12
+FIM_JOGO = false
 
--- aviao_14bis
 aviao_14bis {
     src = "img/nave.png",
-    largura = 64,
-    altura = 64,
+    largura = 55,
+    altura = 63,
     x = LARGURA_TELA / 2 - 64 / 2,
     y = ALTURA_TELA - 64 / 2
 }
+
+function destroiAviao()
+    aviao_14bis.src ="img/explosao_nave.png"
+    aviao_14bis.imagem = love.graphics.newImage(aviao_14bis.src)
+    aviao_14bis.largura = 67
+    aviao_14bis.altura = 77
+end
+
+function temColisao(X1, Y1, L1, A1, X2, Y2, L2, A2)
+    return  X2 < X1 + L1 and
+            X1 < X2 + L2 and
+            Y1 < Y2 + A2 and
+            Y2 < Y1 + A1
+end
 
 meteoros = {}
 
@@ -25,6 +39,8 @@ function criaMeteoro()
     meteoro = {
         x = math.random(LARGURA_TELA),
         y = -80,
+        largura = 50,
+        altura = 44,
         peso = math.random(3),
         deslocamento_horizontal = math.random(-1,1)
     }
@@ -56,6 +72,15 @@ function move14bis()
     end
 end
 
+function checaColisoes()
+    for k, meteoro in pairs(meteoros) do
+        if temColisao(meteoro.x, meteoro.y, meteoro.largura, meteoro.altura, aviao_14bis.x, aviao_14bis.y, aviao_14bis.largura, aviao_14bis.altura) then
+            destroiAviao()
+            FIM_JOGO = true
+        end
+    end
+end
+
 function love.load()
     love.window.setMode(LARGURA_TELA, ALTURA_TELA, {
         resizable = false
@@ -71,17 +96,20 @@ function love.load()
 end
 
 function love.update(dt)
-    if love.keyboard.isDown('w', 'a', 's', 'd') then
-        move14bis()
+    if not FIM_JOGO then
+        if love.keyboard.isDown('w', 'a', 's', 'd') then
+            move14bis()
+        end
+
+        removeMeteoro()
+
+        if #meteoros < MAX_METEORO then
+            criarMeteoro()
+        end
+
+        moveMeteoros()
+        checaColisoes()
     end
-
-    removeMeteoro()
-
-    if #meteoros < MAX_METEORO then
-        criarMeteoro()
-    end
-
-    moveMeteoros()
 end
 
 function love.draw()
